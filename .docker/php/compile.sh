6f1071663d6151020b45b74fe872e3289c50b264
Stephane Moser <moser.sts@gmail.com>
2021-07-01 11:44:19 +0100

    Fix main image name

--- .docker/php/compile.sh
+++ .docker/php/compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build 80-fpm-prod -t ghcr.io/fogospt/fogosapi-php
-docker build 80-fpm-dev -t ghcr.io/fogospt/fogosapi-php:dev
+docker build 80-fpm-prod -t ghcr.io/fogospt/fogosapi
+docker build 80-fpm-dev -t ghcr.io/fogospt/fogosapi:dev
