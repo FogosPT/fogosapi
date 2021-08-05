53299e6f4e094585397784d7b5264eba39c2332f
Tomahock <eu@tomahock.com>
2021-08-05 15:44:40 +0100

    fix last commit

--- app/Http/Controllers/PlanesController.php
+++ app/Http/Controllers/PlanesController.php
@@ -6,11 +6,10 @@ namespace App\Http\Controllers;
 use App\Models\Planes;
 use Illuminate\Http\JsonResponse;
 use Laravel\Lumen\Routing\Controller;
-use Nyholm\Psr7\Request;
 
 class PlanesController extends Controller
 {
-    public function icao(Request $request, $icao)
+    public function icao($icao)
     {
         $plane = Planes::where('icao', $icao)
             ->orderBy('created', 'desc')
