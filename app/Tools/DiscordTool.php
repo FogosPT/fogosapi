6e0452ca51cc3981f1d97d44d147cb76f22c17c9
Tomahock <eu@tomahock.com>
2021-08-12 11:47:41 +0100

    acidente aereo to vost discord

--- app/Tools/DiscordTool.php
+++ app/Tools/DiscordTool.php
@@ -27,4 +27,28 @@ class DiscordTool
         curl_exec($ch);
         curl_close($ch);
     }
+
+    public static function postAero($message)
+    {
+        if (!env('DISCORD_ENABLE')) {
+            return;
+        }
+
+        $webhookId = env('DISCORD_WEBHOOK_ID_AERO');
+        $webhookToken = env('DISCORD_WEBHOOK_TOKEN_AERO');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://discordapp.com/api/webhooks/{$webhookId}/{$webhookToken}");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt(
+            $ch,
+            CURLOPT_POSTFIELDS,
+            "content={$message}"
+        );
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_exec($ch);
+        curl_close($ch);
+    }
 }
