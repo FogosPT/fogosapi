42e5382d56e34c07a8c484e631532fc2f5b62ce9
Tomahock <eu@tomahock.com>
2021-09-22 12:52:01 +0100

    add job to kernel

--- app/Console/Commands/TestStuff.php
+++ app/Console/Commands/TestStuff.php
@@ -55,6 +55,7 @@ class TestStuff extends Command
 
         //$incident = Incident::where('id', "2021080029244")->limit(1)->get()[0];
 
+        dispatch(new UpdateWeatherStations());
         dispatch(new UpdateWeatherData());
 
         //dispatch(new ProcessICNFFireData($incident));
