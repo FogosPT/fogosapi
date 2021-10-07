f8352319bb0378c0e1a2722161948f1ca7bf268b
José Postiga <jpostiga@infraspeak.com>
2021-10-07 12:48:50 +0100

    fix: remove hardcoded sentry DSN

--- config/sentry.php
+++ config/sentry.php
@@ -1,8 +1,8 @@
 <?php
 
-return array(
-    'dsn' => 'https://a09e98456bf44b4baf5c85ef41e93637@o265009.ingest.sentry.io/5806010',
+return [
+    'dsn' => env('SENTRY_DSN', ''),
 
     // capture release as git sha
     // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
-);
+];
