9a8ec7840812f79687f1f72cf9f3d38f3af18873
Tomahock <eu@tomahock.com>
2022-07-13 17:16:23 +0100

    fix ignitions

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -11,24 +11,38 @@ use Laravel\Lumen\Routing\Controller;
 
 class StatsController extends Controller
 {
-    public function ignitionsHourly()
+    public function ignitionsHourly(Request $request)
     {
-        $timestamp = Carbon::today()->startOfDay();
+        $day = $request->get('day');
 
-        $incidents = Incident::where('created', '>', $timestamp)
+        if($day){
+            $day = new Carbon($day);
+        } else {
+            $day = Carbon::today();
+        }
+
+
+        $incidents = Incident::whereBetween(
+                        'dateTime',
+                        [
+                            Carbon::parse($day->startOfDay()),
+                            Carbon::parse($day->endOfDay())
+                        ]
+                        )
                         ->where('isFire', true)
                         ->get();
 
         $hours = [];
 
         foreach ($incidents as $i){
-            $k = (string)$i->created->hour;
+            $k = $i->dateTime->startOfHour()->hour . 'H' . ' - ' . ($i->dateTime->startOfHour()->hour + 1) . 'H';
             if(isset($hours[$k])){
                 $hours[$k]++;
             } else {
                 $hours[$k] = 1;
             }
         }
+        ksort($hours);
 
         return new JsonResponse([
             'success' => true,
