5536badcc8950f6dfdedcdc58336a2d1bbd252cf
Tomahock <eu@tomahock.com>
2022-08-22 16:42:42 +0100

    burn 1000

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,9 @@ $router->group(['prefix' => 'v2'], function () use ($router) {
         $router->get('search', '\App\Http\Controllers\IncidentController@search');
         $router->get('active/kml', '\App\Http\Controllers\IncidentController@activeKML');
         $router->get('active', '\App\Http\Controllers\IncidentController@active');
+        $router->get('1000ha-burned', '\App\Http\Controllers\IncidentController@burnMoreThan1000');
         $router->get('{id}/kml', '\App\Http\Controllers\IncidentController@kml');
+
         $router->post('{id}/posit', '\App\Http\Controllers\IncidentController@addPosit');
         $router->post('{id}/kml', '\App\Http\Controllers\IncidentController@addKML');
     });
