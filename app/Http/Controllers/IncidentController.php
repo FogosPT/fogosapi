2ec0743ee6ca270241eb513405637eca6662bec9
Tomahock <eu@tomahock.com>
2022-11-07 12:11:51 +0000

    fix geojson

--- app/Http/Controllers/IncidentController.php
+++ app/Http/Controllers/IncidentController.php
@@ -223,6 +223,8 @@ class IncidentController extends Controller
 
     private function transformToGeoJSON($data)
     {
+        $features = [];
+        
         foreach($data as $d) {
             $features[] = array(
                 'type' => 'Feature',
